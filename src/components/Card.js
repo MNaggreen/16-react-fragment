911d6db9c271700d56ffbfe18ea56c9b41e790a4
NIKOLAY ORESCHENKO <NICKOLASIO1594@GMAIL.COM>
2022-09-08 22:28:14 +0300

    16-react-fragment

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+const Card = () => {
+  return (
+    //можно в пустой тег добавить React.Fragment это без всяких проблем заменут div
+    <>
+      <h1>Nikolay</h1>
+      <h3>I am learning how to code</h3>
+      <button>Like!</button>
+    </>
+  )
+}
+
+export default Card
